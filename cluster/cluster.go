secant/quilt

provider: Support for DigitalOcean

cluster/cluster.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/NetSys/quilt/cluster/acl"
 	"github.com/NetSys/quilt/cluster/amazon"
+	"github.com/NetSys/quilt/cluster/digitalocean"
 	"github.com/NetSys/quilt/cluster/foreman"
 	"github.com/NetSys/quilt/cluster/google"
 	"github.com/NetSys/quilt/cluster/machine"
@@ -29,7 +30,7 @@ type provider interface {
 }
 
 // Store the providers in a variable so we can change it in the tests
-var allProviders = []db.Provider{db.Amazon, db.Google, db.Vagrant}
+var allProviders = []db.Provider{db.Amazon, db.DigitalOcean, db.Google, db.Vagrant}
 
 type cluster struct {
 	namespace string
@@ -433,6 +434,8 @@ func newProviderImpl(p db.Provider, namespace string) (provider, error) {
 		return amazon.New(namespace)
 	case db.Google:
 		return google.New(namespace)
+	case db.DigitalOcean:
+		return digitalocean.New(namespace)
 	case db.Vagrant:
 		return vagrant.New(namespace)
 	default:

cluster/misc_test.go
@@ -23,6 +23,14 @@ func TestDefaultRegion(t *testing.T) {
 	}
 
 	m.Region = ""
+	m.Provider = "DigitalOcean"
+	exp = "nyc1"
+	m = DefaultRegion(m)
+	if m.Region != exp {
+		t.Errorf("expected %s, found %s", exp, m.Region)
+	}
+
+	m.Region = ""
 	m.Provider = "Google"
 	exp = "us-east1-b"
 	m = DefaultRegion(m)

ejj/kelda

Co-Authored-By: Ethan J. Jackson <ejj@eecs.berkeley.edu>

cluster/cluster.go
@@ -7,6 +7,7 @@ import (
 	log "github.com/Sirupsen/logrus"
 	"github.com/quilt/quilt/cluster/acl"
 	"github.com/quilt/quilt/cluster/amazon"
+	"github.com/quilt/quilt/cluster/digitalocean"
 	"github.com/quilt/quilt/cluster/foreman"
 	"github.com/quilt/quilt/cluster/google"
 	"github.com/quilt/quilt/cluster/machine"
@@ -29,7 +30,7 @@ type provider interface {
 }
 
 // Store the providers in a variable so we can change it in the tests
-var allProviders = []db.Provider{db.Amazon, db.Google, db.Vagrant}
+var allProviders = []db.Provider{db.Amazon, db.DigitalOcean, db.Google, db.Vagrant}
 
 type instance struct {
 	provider db.Provider
@@ -458,6 +459,8 @@ func newProviderImpl(p db.Provider, namespace, region string) (provider, error) 
 		return amazon.New(namespace, region)
 	case db.Google:
 		return google.New(namespace, region)
+	case db.DigitalOcean:
+		return digitalocean.New(namespace, region)
 	case db.Vagrant:
 		return vagrant.New(namespace)
 	default:
@@ -471,6 +474,8 @@ func validRegionsImpl(p db.Provider) []string {
 		return amazon.Regions
 	case db.Google:
 		return google.Zones
+	case db.DigitalOcean:
+		return digitalocean.Regions
 	case db.Vagrant:
 		return []string{""} // Vagrant has no regions
 	default:

cluster/misc_test.go
@@ -23,6 +23,14 @@ func TestDefaultRegion(t *testing.T) {
 	}
 
 	m.Region = ""
+	m.Provider = "DigitalOcean"
+	exp = "sfo1"
+	m = DefaultRegion(m)
+	if m.Region != exp {
+		t.Errorf("expected %s, found %s", exp, m.Region)
+	}
+
+	m.Region = ""
 	m.Provider = "Google"
 	exp = "us-east1-b"
 	m = DefaultRegion(m)